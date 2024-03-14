5075cf01c5c2f5726c35c1672ff87fa527d4cf55
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2024-03-14 10:11:14 -0300

    enhancement(script): adjusts the windows script to kill the process before recreating a new one

--- scripts/windows/StartEmacsServer.bat
+++ scripts/windows/StartEmacsServer.bat
@@ -1,2 +1,9 @@
+@echo off
+REM Kill emacs.exe process
+taskkill /f /im emacs.exe
+
+REM Delete Emacs server files
 del /q %HOME%\.config\emacs\server\*
+
+REM Start Emacs daemon
 runemacs --daemon --chdir %HOME%

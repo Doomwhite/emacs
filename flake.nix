125bc7f1a29fb49ba7dca4b393bfce918159b69a
Doomwhite <doomwhitex@gmail.com>
2025-04-10 16:52:28 -0300

    feat: adds the formatter

--- flake.nix
+++ flake.nix
@@ -6,42 +6,54 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
         # Define Emacs with Tree-sitter grammars
         emacsWithTreesit = (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages (epkgs: [
           epkgs.treesit-grammars.with-all-grammars
         ]);
         # Get the path to the Tree-sitter grammars
         treesitGrammarsPath = "${pkgs.emacsPackages.treesit-grammars.with-all-grammars}/lib";
-      in
-      {
+      in {
         packages.default = emacsWithTreesit.overrideAttrs (old: {
-          buildInput = (old.buildInputs or []) ++ (with pkgs; [
-	    ripgrep
-	    cmake
-	    libtool
-	    rustup
-	    cargo
-	    zls
-	    nodejs
-	    nodePackages.typescript
-	    nodePackages.typescript-language-server
-	    nodePackages."@angular/cli"
-	  ]);
-	  postInstall = (old.postInstall or "") + ''
-            export TREE_SITTER_DIR="${treesitGrammarsPath}"
-            export EMACS_TREESIT_PATH="${treesitGrammarsPath}"
-          '';
-	});
+          buildInput =
+            (old.buildInputs or [])
+            ++ (with pkgs; [
+              ripgrep
+              cmake
+              libtool
+              rustup
+              cargo
+              zls
+              nodejs
+              nodePackages.typescript
+              nodePackages.typescript-language-server
+              nodePackages."@angular/cli"
+            ]);
+
+          postInstall =
+            (old.postInstall or "")
+            + ''
+              export TREE_SITTER_DIR="${treesitGrammarsPath}"
+              export EMACS_TREESIT_PATH="${treesitGrammarsPath}"
+              echo "TESTE" > $out/haha
+            '';
+        });
+
         devShells.default = pkgs.mkShell {
-          buildInputs = [ emacsWithTreesit ];
+          buildInputs = [emacsWithTreesit];
           shellHook = ''
             export TREE_SITTER_DIR=${treesitGrammarsPath}
             export EMACS_TREESIT_PATH=${treesitGrammarsPath}
           '';
         };
-      });
+        formatter = pkgs.alejandra;
+      }
+    );
 }
